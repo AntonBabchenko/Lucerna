c568156917e603ba6c133f2c2c75233928f212c1
Anton <anton.babchenko@outlook.com>
2026-07-08 19:26:36 +0200

    feat(instances): picture entry point in Manage modal + avatar in sidebar picker

--- src/lib/instances/ManageInstancesModal.svelte
+++ src/lib/instances/ManageInstancesModal.svelte
@@ -9,6 +9,7 @@
     type ModCompat,
   } from '$lib/ipc/bindings';
   import IntegritySection from '$lib/instances/IntegritySection.svelte';
+  import { iconDialog } from '$lib/instances/instance-icon-dialog.svelte';
   import { displayLauncher } from '$lib/instances/launcher-display';
   import LoaderPicker from '$lib/instances/LoaderPicker.svelte';
   import MemorySlider from '$lib/instances/MemorySlider.svelte';
@@ -761,6 +762,17 @@
             oninput={() => clearSaved('name')}
             onblur={commitName}
           />
+
+          <div class="mb-3">
+            <button
+              type="button"
+              class="btn-secondary btn-sm"
+              disabled={!selected}
+              onclick={() => selected && iconDialog.show(selected.id, selected.has_icon)}
+            >
+              {$t('instance.icon.changeBtn')}
+            </button>
+          </div>
 
           <label for="detail-mc-version" class="block text-xs text-secondary mb-1"
             >{$t('instance.manage.mcVersionLabel')}</label
